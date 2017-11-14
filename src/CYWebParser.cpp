cd0dc27495b03d231ff0a010963d1edcc78245eb
Adrian <bag4life5@gmail.com>
2017-11-14 23:04:57 +0000

    WebLoader: Wall Regex

--- src/CYWebParser.cpp
+++ src/CYWebParser.cpp
@@ -14,21 +14,20 @@ CYLevel* loadFile(const std::string& levelCode)
     // #name, #levels, #version, #creator
     // Groups: 1 = Name, 2 = Levels, 3 = Version, 4 = Creator
     // Tested with versions 3.68, 3.45, 3.84, 2.13
-    std::regex  reg_header("#name: \"([^\"]+)\", #levels: (\\d+), #version: ([^,]+), #creator: \"([^\"]+)\"");
-    std::smatch sm_header;
+    std::regex reg_header("#name: \"([^\"]+)\", #levels: (\\d+), #version: ([^,]+), #creator: \"([^\"]+)\"");
 
-    if (std::regex_search(levelCode, sm_header, reg_header))
+    if (std::regex_search(levelCode, match_groups, reg_header))
     {
-        sub_match = sm_header[1];
+        sub_match = match_groups[1];
         cyLevel->m_header.name = sub_match.str();
 
-        sub_match = sm_header[2];
+        sub_match = match_groups[2];
         cyLevel->m_header.levels = std::stoi(sub_match.str());
 
-        sub_match = sm_header[3];
+        sub_match = match_groups[3];
         cyLevel->m_header.version = std::stof(sub_match.str());
 
-        sub_match = sm_header[4];
+        sub_match = match_groups[4];
         cyLevel->m_header.author = sub_match.str();
 
     } else {
@@ -41,6 +40,37 @@ CYLevel* loadFile(const std::string& levelCode)
     std::cout << "Levels: " << cyLevel->m_header.levels << std::endl;
     std::cout << "Version: " << cyLevel->m_header.version << std::endl;
     std::cout << "Author: " << cyLevel->m_header.author << std::endl;
+
+    // GAME GEOMETRY
+    // WALLS
+    // [Start_X, Start_Y, Displacement_X, Displacement_Y, [Texture1, Texture2, Z_Index], Level]
+    // Tested with versions 2.13, 3.12, 3.34, 3.59, 3.68, 3.84
+    // TODO: Are there walls with displacement in the decimal direction? (If so please msg me)
+    std::regex reg_walls("\\[(-?\\d+), (-?\\d+), (-?\\d+), (-?\\d+), \\[(c[^\\)]*\\))?(\\d+)?, (c[^\\)]*\\))?(\\d+)?]?, (\\d+)],? ?(\\d+)?");
+
+    std::string wall_code = levelCode;
+    int wall_id = 0;
+    while (std::regex_search(wall_code, match_groups, reg_walls))
+    {
+        std::cout << "Wall ID: " << wall_id << std::endl;
+
+        sub_match = match_groups[1];
+        std::cout << "Start_X: " << sub_match.str() << std::endl;
+
+        sub_match = match_groups[2];
+        std::cout << "Start_Y: " << sub_match.str() << std::endl;
+
+        sub_match = match_groups[3];
+        std::cout << "Displacement_X: " << sub_match.str() << std::endl;
+
+        sub_match = match_groups[4];
+        std::cout << "Displacement_Y: " << sub_match.str() << std::endl;
+
+        wall_id++;
+        wall_code = match_groups.suffix();
+
+        std::cout << "------------------" << std::endl;
+    }
 
     // Return pointer to CYLevel
     return cyLevel;
