9667804e083f83efad56f76f45beea6c8c687c3a
Hopson97 <mhopson@hotmail.co.uk>
2018-05-30 07:56:26 +0100

    fix cmpiler errors from removing files

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,7 +8,7 @@ Game::Game()
 :   m_window    ({1280, 720}, "Space Invaders")
 {
     m_window.setPosition({m_window.getPosition().x, 0});
-    m_window.setFramerateLimit(60);
+   // m_window.setFramerateLimit(60);
     pushState<StatePlaying>(*this);
 }
 
