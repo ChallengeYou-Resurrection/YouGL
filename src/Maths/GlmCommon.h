9a420ff09f9d7d286fd3aa241fd70b1ab082a79c
Adrian <bag4life5@gmail.com>
2018-09-14 00:23:21 +0100

    Updated for GLM 9.9.0+

--- src/Maths/GlmCommon.h
+++ src/Maths/GlmCommon.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #define GLM_ENABLE_EXPERIMENTAL
+#define GLM_FORCE_CTOR_INIT 
 
-#include <glm/glm.hpp>
+#include <glm/common.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/intersect.hpp>
